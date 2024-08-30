8c75f36a32c59c32c58e081cd3810cefc36e1a6e
Hzl <12517398+hzl518@user.noreply.gitee.com>
2024-08-30 22:39:00 +0800

    2024-8-30

---
+++ Code/code/FCFS.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import os
+import json
+import glob
+
+DATAPATH="C:\\Users\Administrator\Desktop\IdsLab\任务\SchedulerSystem\Code\data\*.json"
+data=glob.glob(DATAPATH)
